89b145e3b515b283452071cc6fb16e01b94a5ddb
CasualK <57936400+CasualK@users.noreply.github.com>
2023-02-24 17:59:16 +0900

    Add files via upload

---
+++ mkdata.sql
@@ -0,0 +1,22 @@
+
+CREATE DATABASE lecture;
+USE lecture;
+
+
+CREATE TABLE IF NOT EXISTS `dept` (
+  `DEPTNO` int(11) DEFAULT NULL,
+  `DNAME` varchar(14) DEFAULT NULL,
+  `LOC` varchar(13) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `dept`
+-- lecturelecturelecturelecture-lecture
+
+INSERT INTO `dept` (`DEPTNO`, `DNAME`, `LOC`) VALUES
+(10, 'ACCOUNTING', 'NEW YORK'),
+(20, 'RESEARCH', 'DALLAS'),
+(30, 'SALES', 'CHICAGO'),
+(40, 'OPERATIONS', 'BOSTON');
+
+commit;
